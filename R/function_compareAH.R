16f660de0cac6ec9e6e194e8f4f8fcac5b0c780b
Melissa Bedinger <dr.m.bedinger@gmail.com>
2023-06-12 14:33:27 +0100

    Update compareAH & exportAH_compared functions

--- R/function_compareAH.R
+++ R/function_compareAH.R
@@ -50,32 +50,32 @@ compareAH <- function(AH_benchmark, scenarios_toCompare, scenarioNames) {
   
   # Key points here: 
   
-  # baseline_value_amp and value_amp columns amplify the SBC_norm values by * 100,000
+  # benchmark_value_amp and value_amp columns amplify the SBC_norm values by * 100,000
   # to make them easier to distinguish quickly by eye
   
-  # baseline_value_amp and value_amp columns leave all other metric types out
-  # so e.g. baseline_value_amp and baseline_value are the same for metric == EC
+  # benchmark_value_amp and value_amp columns leave all other metric types out
+  # so e.g. benchmark_value_amp and benchmark_value are the same for metric == EC
   # because EC is already normalized on a 0-1 scale
   
-  # change_pct uses baseline_value_amp and value_amp
+  # change_pct uses benchmark_value_amp and value_amp
   # change_pct is itself amplified * 100 to make it easier to distinguish by eye
   # where a value of 7.5 = 7.5% (not 750%)
   
   results_step2 <-
     AH_benchmark$results %>%
     select(-version, -location, -scenario) %>%
-    rename(baseline_value = value, baseline_rankByLevel = rank_byLevel) %>%
+    rename(benchmark_value = value, benchmark_rankByLevel = rank_byLevel) %>%
     full_join(results_step1, 
               by = c("level", "levelName_full", "levelName", "Node", "metric")) %>%
-    mutate(baseline_value_amp = 
-             ifelse(metric == "SBC_norm", baseline_value * 100000, baseline_value), # SBC_norm values are amplified here by 100,000 to make them easier to distinguish by eye
+    mutate(benchmark_value_amp = 
+             ifelse(metric == "SBC_norm", benchmark_value * 100000, benchmark_value), # SBC_norm values are amplified here by 100,000 to make them easier to distinguish by eye
            value_amp = 
              ifelse(metric == "SBC_norm", value * 100000, value), # SBC_norm values are amplified here by 100,000 to make them easier to distinguish by eye
-           change_value_amp = value_amp - baseline_value_amp,
+           change_value_amp = value_amp - benchmark_value_amp,
            change_pct = 
-             ifelse(baseline_value == 0 & value == 0, 0,
-                    ((value_amp - baseline_value_amp) / baseline_value_amp) * 100), # Not that changePct is after * 100 for % value
-           change_rankByLevel = baseline_rankByLevel - rank_byLevel) # Note that a smaller rank number signifies a higher rank; a positive change_rank number signifies an increase in rank
+             ifelse(benchmark_value == 0 & value == 0, 0,
+                    ((value_amp - benchmark_value_amp) / benchmark_value_amp) * 100), # Not that changePct is after * 100 for % value
+           change_rankByLevel = benchmark_rankByLevel - rank_byLevel) # Note that a smaller rank number signifies a higher rank; a positive change_rank number signifies an increase in rank
 
   if(any(sapply(scenarios_toCompare, function(x) any(names(x) == "confidence_rankByLevel_minusPlus")))) {
     
@@ -84,7 +84,7 @@ compareAH <- function(AH_benchmark, scenarios_toCompare, scenarioNames) {
       select(
         scenarioName, name, version, location, scenario, date, # scenario identifiers
         level, levelName_full, levelName, Node, # basic identifiers
-        metric, baseline_value, baseline_value_amp, baseline_rankByLevel, # baseline results
+        metric, benchmark_value, benchmark_value_amp, benchmark_rankByLevel, # baseline / benchmark results
         value, value_amp, change_value_amp, change_pct, # scenario value change
         rank_byLevel, change_rankByLevel, confidence_rankByLevel_minusPlus, # scenario rank change
         confidence_rankByLevel_minus, rank_byLevel_minus, # sensitivity rank for detail on confidence (minus)
@@ -107,7 +107,7 @@ compareAH <- function(AH_benchmark, scenarios_toCompare, scenarioNames) {
       select(
         scenarioName, name, version, location, scenario, date, # scenario identifiers
         level, levelName_full, levelName, Node, # basic identifiers
-        metric, baseline_value, baseline_value_amp, baseline_rankByLevel, # baseline results
+        metric, benchmark_value, benchmark_value_amp, benchmark_rankByLevel, # baseline / benchmark results
         value, value_amp, change_value_amp, change_pct, # scenario value change
         rank_byLevel, change_rankByLevel) # scenario rank change
   }
---
+++ R/function_exportAH_compared.R
@@ -0,0 +1,30 @@
+exportAH_compared <- function(AH_compared, directory = NULL, 
+                              name, version, benchmark, scenarios) {
+  
+  filenameRDS <- 
+    filenameTimestamp(
+      prefix = paste0(
+        directory, 
+        "comparison_", name, "_", version, "_", benchmark, "-", scenarios),
+      extension = ".RDS")
+  
+  AH_compared %>% saveRDS(filenameRDS)
+  
+  message(paste0("Files saved as: "))
+  
+  message(paste0("\n\t", filenameRDS))
+  
+  filenameXLSX <- 
+    filenameTimestamp(
+      prefix = paste0(
+        directory, 
+        "comparison_", name, "_", version, "_", benchmark, "-to-", scenarios),
+      extension = ".xlsx")
+  
+  AH_compared$results %>% write.xlsx(filenameXLSX)
+  
+  message(paste0("Files saved as: "))
+  
+  message(paste0("\n\t", filenameXLSX))
+  
+}
