3b1304c9c2ea5e3363f6974135084e0289b460aa
Melissa Bedinger <dr.m.bedinger@gmail.com>
2023-06-16 14:59:33 +0100

    Update example data

--- data-raw/preProcess.R
+++ data-raw/preProcess.R
@@ -1,6 +1,6 @@
 setwd(dirname(rstudioapi::getSourceEditorContext()$path)); getwd()
+library(tidyverse)
+colsFloodRiver_df <- 
+  read_xlsx("aes_colsFloodRiver_df.xlsx")
 
-USAH_3.0_template_baseline_vInfo_full <- 
-  read_xlsx("USAH_3.0_template_baseline_vInfo-full_20230608.xlsx")
-
-usethis::use_data(USAH_3.0_template_baseline_vInfo_full)
+usethis::use_data(colsFloodRiver_df)
