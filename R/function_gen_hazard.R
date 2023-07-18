b330d8c19cbb49fdcb34a7d1c42779bfda0bc5bf
Melissa Bedinger <dr.m.bedinger@gmail.com>
2023-07-18 11:41:39 +0100

    Update functions & vignettes

--- R/function_gen_hazard.R
+++ R/function_gen_hazard.R
@@ -1,59 +1,62 @@
-# Generate edgesNew based on proportion of physical objects still functional 
+# Generate edgesNew based on proportion of Resources i.e. Physical objects still functional 
 # as represented in a countCompared OSMtidy output
-gen_hazard <- function(vInfo_template_full, edgelist, 
-                       countCompared, hazard = NA, proxyWeight = 0) {
+gen_hazard <- function(vInfo_template_full, 
+                       edgelist, 
+                       countCompared, 
+                       hazard = NA, 
+                       proxyWeight = 0) {
   
   # Create vector of physical objects affected
-  objectsAffected <-
-    countCompared$compare_byresource %>% 
-    filter(!is.na(total_prop)) %>% # Filter out results for objects that were not detected in the second scenario
-    pull(resource)
+  resourcesAffected <-
+    countCompared$compare_byPhysicalObject %>% 
+    filter(!is.na(total_prop)) %>% # Filter out results for Resources that were not detected in the second scenario
+    pull(physicalObject)
   
   if (hazard == "drought") {
     
-    # Create check for affected physical objects by hazard type (drought)
-    objectsNotAffected_drought <-
+    # Create check for affected Resources by hazard type (drought)
+    resourcesNotAffected_drought <-
       vInfo_template_full %>% 
       filter(level == 5) %>% 
       filter(subnetwork_hazard_drought == FALSE) %>%
       pull(Node)
     
     # Update 
-    objectsAffected <- 
-      objectsAffected[!objectsAffected %in% objectsNotAffected_drought]
+    resourcesAffected <- 
+      resourcesAffected[!resourcesAffected %in% resourcesNotAffected_drought]
     
   }
   
   if (hazard == "flood") {
     
-    # Create check for affected physical objects by hazard type (flood)
-    objectsNotAffected_flood <-
+    # Create check for affected Resources by hazard type (flood)
+    resourcesNotAffected_flood <-
       vInfo_template_full %>% 
       filter(level == 5) %>% 
       filter(subnetwork_hazard_flood == FALSE) %>%
       pull(Node)
     
-    # Update objectsAffected
-    objectsAffected <- 
-      objectsAffected[!objectsAffected %in% objectsNotAffected_flood]
+    # Update resourcesAffected
+    resourcesAffected <- 
+      resourcesAffected[!resourcesAffected %in% resourcesNotAffected_flood]
     
   }
   
-  # Create dataframe of weightNew for physical objects affected
-  objectsAffected_weights <-
+  # Create dataframe of weightNew for Resources affected
+  resourcesAffected_weights <-
     countCompared$compare_byPhysicalObject %>% # Extract compare_byPhysicalObject
-    filter(!is.na(total_prop)) %>% # Filter out results for objects that were not detected in the second scenario
-    filter(physicalObject %in% objectsAffected) %>% # Use updated objectsAffected for hazard scenarios to use only weights for these objects
+    filter(!is.na(total_prop)) %>% # Filter out results for Resources that were not detected in the second scenario
+    filter(physicalObject %in% resourcesAffected) %>% # Use updated resourcesAffected for hazard scenarios to use only weights for these objects
     rename(to = physicalObject, weightNew = total_prop) %>% # Rename for joining to edgelist_input
-    mutate(weightNew = replace(weightNew, weightNew == 0, proxyWeight)) %>% # Objects for complete removal will = 0, replace this with proxyWeight
+    mutate(weightNew = replace(weightNew, weightNew == 0, proxyWeight)) %>% # Resources for complete removal will = 0, replace this with proxyWeight
     mutate(weightNew = replace(weightNew, weightNew < 0.0000000001, proxyWeight)) %>% # Weights assigned by OSMtidy outputs which are effectively removed should be set to the proxyWeight
     select(to, weightNew)
   
   # Join dataframes
   edgesNew <-
     edgelist %>%
-    filter(to %in% objectsAffected) %>% # Filter edgelist_input to only physical objects affected
-    left_join(objectsAffected_weights, by = "to") %>% # Join
+    filter(to %in% resourcesAffected) %>% # Filter edgelist_input to only Resources affected
+    left_join(resourcesAffected_weights, by = "to") %>% # Join
     select(-weight) # Remove weight column now superseded by weightNew
   
   return(edgesNew)
