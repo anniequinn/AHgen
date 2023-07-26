084196207fb02a1eb1d5c60d7393eb9f099fd071
Melissa Bedinger <dr.m.bedinger@gmail.com>
2023-07-26 17:21:59 +0100

    Tidy

--- archive/R/function_preview_DEPRECATED.R
+++
@@ -1,3 +0,0 @@
-preview <- function(input) { 
-  input %>% str(list.len = 10, width = 100, strict.width = "cut")
-  }
--- archive/R/functions_internal_vis_layout_DEPRECATED.R
+++
@@ -1,8 +0,0 @@
-visLayout <- function(edgelist, vInfo, minSpacing = 0, maxSpacing = 100, key) { 
-    
-    internal_horizontalLayout(edgelist = edgelist, vInfo = vInfo) %>%
-      internal_horizontalLayout_spacing(., minSpacing = minSpacing, maxSpacing = maxSpacing) %>%
-      internal_radialLayout(horizontalLayout = ., edgelist = edgelist, key = key) %>% 
-      internal_genericLayout(radialLayout = .)
-    
-  }
