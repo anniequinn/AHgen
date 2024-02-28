d2bb2025600e093a8fc66c2c94eb07e9ad26493f
Melissa Bedinger <dr.m.bedinger@gmail.com>
2024-02-28 14:48:06 +0000

    Update function_vis_plotViolin.R

--- R/function_vis_plotViolin.R
+++ R/function_vis_plotViolin.R
@@ -143,20 +143,24 @@ vis_plotViolin <- function(results,
       
       violinPlot <-
         ggplot(data = results) +
-        aes(x = levelName, y = value, fill = scenario) +
+        aes(x = levelName, y = value, fill = scenarioName) +
         see::geom_violinhalf(
-          data = results %>% filter(scenario == "baseline"), 
+          data = 
+            results %>% 
+            filter(scenarioName == "UK"), 
           alpha = 0.7, aes(fill = "#56B4E9"), flip = TRUE, linewidth = 0.1) + # using default colours from AHgen package; flipped
         see::geom_violinhalf(
-          data = results %>% filter(scenario == "Rawa Terate offline"), 
+          data = 
+            results %>% 
+            filter(scenarioName == "Indonesia"), 
           alpha = 0.7, aes(fill = "#85929E"), linewidth = 0.1) + # using default colours from AHgen package
         ggh4x::facet_grid2(
           independent = "x", levelName ~ location, 
           labeller = label_wrap_gen(width = 15), scales = "free", switch = "y") +
         labs(y = yaxisLab) +
-        scale_fill_manual(name = "scenario", 
+        scale_fill_manual(name = "country", 
                           values = levels(results$cols), 
-                          labels = levels(results$scenario)) +
+                          labels = levels(results$scenarioName)) +
         scale_y_continuous(limits = c(0, NA), position = "right") +
         theme(axis.text.x = element_blank(),
               axis.text.y = element_text(color = "grey40", size = 24),
@@ -192,48 +196,146 @@ vis_plotViolin <- function(results,
     
   } else if(type == "overlap") {
     
-    violinPlot <-
-      ggplot(data = results) +
-      aes(x = levelName, y = value, color = scenario) +
-      geom_violin(alpha = 0.1, position = "identity", linewidth = 0.2) +
-      ggh4x::facet_grid2(
-        independent = "x", levelName ~ location, 
-        labeller = label_wrap_gen(width = 15), scales = "free", switch = "y") +
-      labs(y = yaxisLab) +
-      scale_color_manual(name = "scenario", 
-                         values = levels(results$cols), 
-                         labels = levels(results$scenario)) + # using default colours from AHgen package
-      scale_y_continuous(limits = c(0, NA), position = "right") +
-      guides(color = guide_legend(override.aes = list(linewidth = 2))) +
-      theme(axis.text.x = element_blank(),
-            axis.text.y = element_text(color = "grey40", size = 24),
-            axis.ticks.x = element_blank(),
-            axis.ticks.y = element_line(color = "grey90", linewidth = 0.2),
-            axis.title.x = element_blank(),
-            axis.title.y = element_text(color = "grey40", size = 32),
-            legend.box.margin = margin(-4, -4, -4, -4),
-            legend.key.size = unit(0.4, "line"),
-            legend.justification = "center",
-            legend.margin = margin(c(2, 0, 4, 0)),
-            legend.position = "bottom",
-            legend.spacing.x = unit(1.4, 'mm'),
-            legend.text = element_text(color = "grey25", size = 32),
-            legend.title = element_text(color = "grey25", size = 36),
-            panel.background = element_rect(color = "gray85", fill = "gray98"),
-            panel.border = element_blank(),
-            panel.grid.major.x = element_blank(),
-            panel.grid.major.y = element_line(color = "gray90", linewidth = 0.1),
-            panel.grid.minor.x = element_blank(),
-            panel.grid.minor.y = element_blank(),
-            panel.spacing = unit(0.3, "lines"),
-            plot.background = element_rect(fill = "white"),
-            plot.margin = grid::unit(c(2, 4, 2, 0), "mm"),
-            plot.title = element_blank(),
-            strip.background = element_rect(fill = "white"),
-            strip.text.x = element_text(color = "grey25", lineheight = 2, size = 34),
-            strip.text.y.left = element_text(
-              angle = 0, color = "grey25", lineheight = 0.15, size = 34),
-            text = element_text(family = family))
+    if(study == "flood") {
+      
+      violinPlot <-
+        ggplot(data = results) +
+        aes(x = levelName, y = value, color = scenario) +
+        geom_violin(alpha = 0.1, position = "identity", linewidth = 0.2) +
+        ggh4x::facet_grid2(
+          independent = "x", levelName ~ location, 
+          labeller = label_wrap_gen(width = 15), scales = "free", switch = "y") +
+        labs(y = yaxisLab) +
+        scale_color_manual(name = "scenario", 
+                           values = levels(results$cols), 
+                           labels = levels(results$scenario)) + # using default colours from AHgen package
+        scale_y_continuous(limits = c(0, NA), position = "right") +
+        guides(color = guide_legend(override.aes = list(linewidth = 2))) +
+        theme(axis.text.x = element_blank(),
+              axis.text.y = element_text(color = "grey40", size = 24),
+              axis.ticks.x = element_blank(),
+              axis.ticks.y = element_line(color = "grey90", linewidth = 0.2),
+              axis.title.x = element_blank(),
+              axis.title.y = element_text(color = "grey40", size = 32),
+              legend.box.margin = margin(-4, -4, -4, -4),
+              legend.key.size = unit(0.4, "line"),
+              legend.justification = "center",
+              legend.margin = margin(c(2, 0, 4, 0)),
+              legend.position = "bottom",
+              legend.spacing.x = unit(1.4, 'mm'),
+              legend.text = element_text(color = "grey25", size = 32),
+              legend.title = element_text(color = "grey25", size = 36),
+              panel.background = element_rect(color = "gray85", fill = "gray98"),
+              panel.border = element_blank(),
+              panel.grid.major.x = element_blank(),
+              panel.grid.major.y = element_line(color = "gray90", linewidth = 0.1),
+              panel.grid.minor.x = element_blank(),
+              panel.grid.minor.y = element_blank(),
+              panel.spacing = unit(0.3, "lines"),
+              plot.background = element_rect(fill = "white"),
+              plot.margin = grid::unit(c(2, 4, 2, 0), "mm"),
+              plot.title = element_blank(),
+              strip.background = element_rect(fill = "white"),
+              strip.text.x = element_text(color = "grey25", lineheight = 2, size = 34),
+              strip.text.y.left = element_text(
+                angle = 0, color = "grey25", lineheight = 0.15, size = 34),
+              text = element_text(family = family))
+      
+    }
+    
+    if(study == "offline") {
+      
+      violinPlot <-
+        ggplot(data = results) +
+        aes(x = levelName, y = value, color = scenario) +
+        geom_violin(alpha = 0.1, position = "identity", linewidth = 0.2) +
+        ggh4x::facet_grid2(
+          independent = "x", levelName ~ location, 
+          labeller = label_wrap_gen(width = 15), scales = "free", switch = "y") +
+        labs(y = yaxisLab) +
+        scale_color_manual(name = "scenario", 
+                           values = levels(results$cols), 
+                           labels = levels(results$scenario)) + # using default colours from AHgen package
+        scale_y_continuous(limits = c(0, NA), position = "right") +
+        guides(color = guide_legend(override.aes = list(linewidth = 2))) +
+        theme(axis.text.x = element_blank(),
+              axis.text.y = element_text(color = "grey40", size = 24),
+              axis.ticks.x = element_blank(),
+              axis.ticks.y = element_line(color = "grey90", linewidth = 0.2),
+              axis.title.x = element_blank(),
+              axis.title.y = element_text(color = "grey40", size = 32),
+              legend.box.margin = margin(-4, -4, -4, -4),
+              legend.key.size = unit(0.4, "line"),
+              legend.justification = "center",
+              legend.margin = margin(c(2, 0, 4, 0)),
+              legend.position = "bottom",
+              legend.spacing.x = unit(1.4, 'mm'),
+              legend.text = element_text(color = "grey25", size = 32),
+              legend.title = element_text(color = "grey25", size = 36),
+              panel.background = element_rect(color = "gray85", fill = "gray98"),
+              panel.border = element_blank(),
+              panel.grid.major.x = element_blank(),
+              panel.grid.major.y = element_line(color = "gray90", linewidth = 0.1),
+              panel.grid.minor.x = element_blank(),
+              panel.grid.minor.y = element_blank(),
+              panel.spacing = unit(0.3, "lines"),
+              plot.background = element_rect(fill = "white"),
+              plot.margin = grid::unit(c(2, 4, 2, 0), "mm"),
+              plot.title = element_blank(),
+              strip.background = element_rect(fill = "white"),
+              strip.text.x = element_text(color = "grey25", lineheight = 2, size = 34),
+              strip.text.y.left = element_text(
+                angle = 0, color = "grey25", lineheight = 0.15, size = 34),
+              text = element_text(family = family))
+      
+    }
+    
+    if(study == "international") {
+      
+      violinPlot <-
+        ggplot(data = results) +
+        aes(x = levelName, y = value, color = scenarioName) +
+        geom_violin(alpha = 0.1, position = "identity", linewidth = 0.2) +
+        ggh4x::facet_grid2(
+          independent = "x", levelName ~ location, 
+          labeller = label_wrap_gen(width = 15), scales = "free", switch = "y") +
+        labs(y = yaxisLab) +
+        scale_color_manual(name = "country", 
+                           values = levels(results$cols), 
+                           labels = levels(results$scenarioName)) + # using default colours from AHgen package
+        scale_y_continuous(limits = c(0, NA), position = "right") +
+        guides(color = guide_legend(override.aes = list(linewidth = 2))) +
+        theme(axis.text.x = element_blank(),
+              axis.text.y = element_text(color = "grey40", size = 24),
+              axis.ticks.x = element_blank(),
+              axis.ticks.y = element_line(color = "grey90", linewidth = 0.2),
+              axis.title.x = element_blank(),
+              axis.title.y = element_text(color = "grey40", size = 32),
+              legend.box.margin = margin(-4, -4, -4, -4),
+              legend.key.size = unit(0.4, "line"),
+              legend.justification = "center",
+              legend.margin = margin(c(2, 0, 4, 0)),
+              legend.position = "bottom",
+              legend.spacing.x = unit(1.4, 'mm'),
+              legend.text = element_text(color = "grey25", size = 32),
+              legend.title = element_text(color = "grey25", size = 36),
+              panel.background = element_rect(color = "gray85", fill = "gray98"),
+              panel.border = element_blank(),
+              panel.grid.major.x = element_blank(),
+              panel.grid.major.y = element_line(color = "gray90", linewidth = 0.1),
+              panel.grid.minor.x = element_blank(),
+              panel.grid.minor.y = element_blank(),
+              panel.spacing = unit(0.3, "lines"),
+              plot.background = element_rect(fill = "white"),
+              plot.margin = grid::unit(c(2, 4, 2, 0), "mm"),
+              plot.title = element_blank(),
+              strip.background = element_rect(fill = "white"),
+              strip.text.x = element_text(color = "grey25", lineheight = 2, size = 34),
+              strip.text.y.left = element_text(
+                angle = 0, color = "grey25", lineheight = 0.15, size = 34),
+              text = element_text(family = family))
+      
+    }
     
   }
   
